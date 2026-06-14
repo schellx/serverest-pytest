db3d95f126062c00a889b0be5392283a40957a75
Casper Schell <1003kasper@gmail.com>
2026-06-14 19:03:25 -0300

    inicial

---
+++ tests/tests_usuarios.py
@@ -0,0 +1,5 @@
+import requests
+import pytest
+from utils.gera_dados import gerar_email
+
+BASE_URL = "https://compassuol.serverest.dev"
---
+++ utils/gera_dados.py
@@ -0,0 +1,4 @@
+import uuid
+
+def gerar_email():
+    return f"usuario_{uuid.uuid4()}@teste.com"
